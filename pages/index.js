60d8add952de5cd1cc1fb9b409178a6fc17e55d9
Ron111104 <rc071011ggmu@gmail.com>
2024-12-13 19:12:06 +0530

    added analytics

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Hero from '@components/Hero';
 import Features from '@components/Features';
 import Testimonials from '@components/Testimonials';
 import Footer from '@components/Footer';
- // Import the Chatbot component
+import Chatbot from '@components/chatBot';
 
 
 export default function Home() {
